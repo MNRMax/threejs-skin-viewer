b52b8018fee9f6f5e90efe418de9ab857b94be04
MNRMax <maximilian.shabatayev@gmail.com>
2022-11-14 11:06:46 -0500

    jfisgio

--- three-js/index.js
+++ three-js/index.js
@@ -1,7 +1,7 @@
 import "./style.css"
 import * as THREE from "three"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { addStar, renderBody, renderHead, renderHelmet, renderLeftArm, renderLeftLeg, renderRightArm, renderRightLeg } from "./renders.js";
+import { addStar, renderBody, renderHead, renderHelmet, renderLeftArm, renderLeftLeg, renderRightArm, renderRightLeg, renderRightSleeve, renderLeftSleeve, renderBodySecondLayer, renderLeftLegSleeve } from "./renders.js";
 
 //setup
 var clock = new THREE.Clock();
@@ -16,29 +16,45 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30);
 
-const skin = "skin.png"
+document.querySelector('#submit').addEventListener("click", function() {
+    console.log('test')
+    let rawData = get(`https://api.ashcon.app/mojang/v2/user/${document.querySelector("#name").value}`)
+    let data = JSON.parse(rawData)
+    renderSkin(`https://crafatar.com/skins/${data.uuid}?overlay&default=MHF_SAlex`)
+})
 
-const head = renderHead(skin)
-const body = renderBody(skin)
-const rightArm = renderRightArm(skin)
-const rightLeg = renderRightLeg(skin)
-const leftArm = renderLeftArm(skin)
-const leftLeg = renderLeftLeg(skin)
-const helmet = renderHelmet(skin)
-scene.add(rightArm, leftArm, rightLeg, head, body, leftLeg, helmet)
+function renderSkin(skin) {
+    scene.clear()
+    scene.add(new THREE.AmbientLight(0xFFFFFF))
+    Array(200).fill().forEach(function() {scene.add(addStar())})
+    
+    const head = renderHead(skin)
+    const body = renderBody(skin)
+    const BodySecondLayer = renderBodySecondLayer(skin)
+    const rightArm = renderRightArm(skin)
+    const rightLeg = renderRightLeg(skin)
+    const leftArm = renderLeftArm(skin)
+    const leftLeg = renderLeftLeg(skin)
+    const LeftLegSleeve = renderLeftLegSleeve(skin)
+    const helmet = renderHelmet(skin)
+    const rightSleeve = renderRightSleeve(skin)
+    const leftSleeve = renderLeftSleeve(skin)
+    scene.add(rightArm, leftArm, rightLeg, head, body, leftLeg, helmet, rightSleeve, leftSleeve, BodySecondLayer, LeftLegSleeve)
+}
+
 
 //light
 const pointLight = new THREE.AmbientLight(0xFFFFFF)
 scene.add(pointLight)
 
 const controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableZoom = false;
-// controls.enablePan = false;
-// camera.setFocalLength(5)
+controls.enableZoom = false;
+controls.enablePan = false;
+camera.setFocalLength(5)
 
 function animate() {
     requestAnimationFrame( animate)
-    // cube.rotation.y += 2 * clock.getDelta();
+    cube.rotation.y += 2 * clock.getDelta();
     controls.update()
     renderer.render(scene, camera);
 }
@@ -47,5 +63,11 @@ const spaceTexture = new THREE.TextureLoader().load('space.jpg')
 scene.background = spaceTexture;
 
 Array(200).fill().forEach(function() {scene.add(addStar())})
-
 animate()
+
+function get(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
--- three-js/renders.js
+++ three-js/renders.js
@@ -171,7 +171,7 @@ export function renderLeftLeg(url) {
 }
 
 export function renderHelmet(url) {
-    const cubeGeometry = new THREE.BoxGeometry(9, 9, 9);
+    const cubeGeometry = new THREE.BoxGeometry(8.5, 8.5, 8.5);
     var headTextures = Array(6).fill().map(() => new THREE.TextureLoader().load(url));
 
     headTextures.forEach(function (e) {
@@ -186,13 +186,130 @@ export function renderHelmet(url) {
     headTextures[4].offset.set(0.625, 0.75)
     headTextures[5].offset.set(0.875, 0.75)
 
-    const materials2 = headTextures.map(e => new THREE.MeshStandardMaterial({ map: e, transparent: true,  side: THREE.DoubleSide }))
     const materials = headTextures.map(e => new THREE.MeshStandardMaterial({ map: e, transparent: true, side: THREE.DoubleSide }))
     const cube = new THREE.Mesh(cubeGeometry, materials)
-    const cube2 = new THREE.Mesh(cubeGeometry, materials2)
-    return cube, cube2;
+    return cube;
 }
 
+export function renderRightSleeve(url) {
+    const cubeGeometry = new THREE.BoxGeometry(4.5, 12.5, 4.5);
+
+    var bodyTextures = Array(6).fill().map(() => new THREE.TextureLoader().load(url));
+
+    bodyTextures.forEach(function (e) {
+        e.magFilter = THREE.NearestFilter;
+    })
+
+    bodyTextures[0].offset.set(0.75, 0.25)
+    bodyTextures[0].repeat.set(0.0625, 0.1875);
+    bodyTextures[1].offset.set(0.625, 0.25)
+    bodyTextures[1].repeat.set(0.0625, 0.1875);
+
+    bodyTextures[2].offset.set(0.6875, 0.4375)
+    bodyTextures[2].repeat.set(0.0625, 0.0625);
+    bodyTextures[3].offset.set(0.75, 0.4375)
+    bodyTextures[3].repeat.set(0.0625, 0.0625);
+
+    bodyTextures[4].offset.set(0.6875, 0.25)
+    bodyTextures[4].repeat.set(0.0625, 0.1875);
+    bodyTextures[5].offset.set(0.8125, 0.25)
+    bodyTextures[5].repeat.set(0.0625, 0.1875);
+
+    const materials = bodyTextures.map(e => new THREE.MeshStandardMaterial({ map: e, transparent: true, side: THREE.DoubleSide }))
+    const cube = new THREE.Mesh(cubeGeometry, materials)
+    cube.position.set(-6, -10, 0)
+    return cube;
+}
+
+export function renderLeftSleeve(url) {
+    const cubeGeometry = new THREE.BoxGeometry(4.5, 12.5, 4.5);
+
+    var bodyTextures = Array(6).fill().map(() => new THREE.TextureLoader().load(url));
+
+    bodyTextures.forEach(function (e) {
+        e.magFilter = THREE.NearestFilter;
+    })
+
+    bodyTextures[0].offset.set(0.875, 0)
+    bodyTextures[0].repeat.set(0.0625, 0.1875);
+    bodyTextures[1].offset.set(0.75, 0)
+    bodyTextures[1].repeat.set(0.0625, 0.1875);
+
+    bodyTextures[2].offset.set(0.8125, 0.1875)
+    bodyTextures[2].repeat.set(0.0625, 0.0625);
+    bodyTextures[3].offset.set(0.875, 0.1875)
+    bodyTextures[3].repeat.set(0.0625, 0.0625);
+
+    bodyTextures[4].offset.set(0.8125, 0)
+    bodyTextures[4].repeat.set(0.0625, 0.1875);
+    bodyTextures[5].offset.set(0.9375, 0)
+    bodyTextures[5].repeat.set(0.0625, 0.1875);
+
+    const materials = bodyTextures.map(e => new THREE.MeshStandardMaterial({ map: e, transparent: true, side: THREE.DoubleSide  }))
+    const cube = new THREE.Mesh(cubeGeometry, materials)
+    cube.position.set(6, -10, 0)
+    return cube;
+}
+
+export function renderBodySecondLayer(url) {
+    const cubeGeometry = new THREE.BoxGeometry(8.5, 12.5, 4.5);
+
+    var bodyTextures = Array(6).fill().map(() => new THREE.TextureLoader().load(url));
+
+    bodyTextures.forEach(function (e) {
+        e.magFilter = THREE.NearestFilter;
+    })
+
+    bodyTextures[0].offset.set(0.25, 0.25)
+    bodyTextures[0].repeat.set(0.0625, 0.1875);
+    bodyTextures[1].offset.set(0.25, 0.25)
+    bodyTextures[1].repeat.set(0.0625, 0.1875);
+
+    bodyTextures[2].offset.set(0.3125, 0.4375)
+    bodyTextures[2].repeat.set(0.125, 0.0625);
+    bodyTextures[3].offset.set(0.4375, 0.4375)
+    bodyTextures[3].repeat.set(0.125, 0.0625);
+
+    bodyTextures[4].offset.set(0.3125, 0.25)
+    bodyTextures[4].repeat.set(0.125, 0.1875);
+    bodyTextures[5].offset.set(0.5, 0.25)
+    bodyTextures[5].repeat.set(0.125, 0.1875);
+
+    const materials = bodyTextures.map(e => new THREE.MeshStandardMaterial({ map: e, transparent: true, side: THREE.DoubleSide }))
+    const cube = new THREE.Mesh(cubeGeometry, materials)
+    cube.position.set(0, -10, 0)
+    return cube;
+}
+
+export function renderLeftLegSleeve(url) {
+    const cubeGeometry = new THREE.BoxGeometry(4.5, 12.5, 4.5);
+
+    var bodyTextures = Array(6).fill().map(() => new THREE.TextureLoader().load(url));
+
+    bodyTextures.forEach(function (e) {
+        e.magFilter = THREE.NearestFilter;
+    })
+
+    bodyTextures[0].offset.set(0.125, 0)
+    bodyTextures[0].repeat.set(0.0625, 0.1875);
+    bodyTextures[1].offset.set(0, 0)
+    bodyTextures[1].repeat.set(0.0625, 0.1875);
+
+    bodyTextures[2].offset.set(0.0625, 0.1875)
+    bodyTextures[2].repeat.set(0.0625, 0.0625);
+    bodyTextures[3].offset.set(0.125, 0.1875)
+    bodyTextures[3].repeat.set(0.0625, 0.0625);
+
+    bodyTextures[4].offset.set(0.0625, 0)
+    bodyTextures[4].repeat.set(0.0625, 0.1875);
+    bodyTextures[5].offset.set(0.1875, 0)
+    bodyTextures[5].repeat.set(0.0625, 0.1875);
+
+    const materials = bodyTextures.map(e => new THREE.MeshStandardMaterial({ map: e, transparent: true, side: THREE.DoubleSide }))
+    const cube = new THREE.Mesh(cubeGeometry, materials)
+    cube.position.set(2, -22, 0)
+    return cube;
+}
 
 export function addStar() {
     const starGeometry = new THREE.SphereGeometry(0.25, 24, 24);
